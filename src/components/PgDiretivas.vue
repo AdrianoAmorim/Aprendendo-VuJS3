00d6ba76207ff19ed489ce6b23175cc7493bad18
Rafael Silveira Gomes <silveiragomesr@gmail.com>
2022-04-20 14:58:30 -0300

    simplifica codigo e corrige o problema dos ifs

--- src/components/PgDiretivas.vue
+++ src/components/PgDiretivas.vue
@@ -3,16 +3,18 @@
   <div>
     <span>DIGITE SEU NOME:</span>
     <input type="text" v-model="inNome" placeholder="Digite seu Nome" />
-    <input type="submit" value="Enviar" @click.prevent="getNome" />
+    <input type="submit" value="Enviar" @click="getNome" />
   </div>
   <p>Oi meu nome é: {{ nome }}</p>
   <p>Estado de Ocupação:</p>
- 
-  <p v-if="status == ''">...</p>
-  <p v-else-if="status">Infelismente esta pessoa esta ocupada !!</p>
-  <p v-else-if="status == 'celular'">Infelismente esta pessoa esta ocupada NO CELULAR!!</p>
-  <p v-else-if="!status">LIBERADO PARA BRINCAR!! PAPAI te AMa</p>
-  <p v-else> ESTA PESSOA NAO ESTA CADASTRADO NA BASE DE DADOS.</p>
+
+  <p v-if="status === ''">...</p>
+  <p v-else-if="status == false">Infelismente esta pessoa esta ocupada !!</p>
+  <p v-else-if="status == 'celular'">
+    Infelismente esta pessoa esta ocupada NO CELULAR!!
+  </p>
+  <p v-else-if="status == true">LIBERADO PARA BRINCAR!! PAPAI te AMa</p>
+  <p v-else>ESTA PESSOA NAO ESTA CADASTRADO NA BASE DE DADOS.</p>
 </template>
 
 <script>
@@ -21,25 +23,24 @@ export default {
   methods: {
     pesquisaNome() {
       if (this.nome == "adriano") {
-   
-        return true;
+        this.status = true;
       } else if (this.nome == "cristiane") {
-      
-        return "celular";
+        this.status = "celular";
       } else if (this.nome == "alice") {
-       
-        return false;
+        this.status = false;
+      }
+      else {
+        this.status = null;
       }
     },
     getNome() {
       this.nome = this.inNome;
-      this.status = this.pesquisaNome();
-      console.log(this.status)
-    }
+      this.pesquisaNome();
+    },
   },
   data() {
     return {
-      status: '',
+      status: "",
       nome: "",
       inNome: "",
     };
